4f25889efec3499e4c76f6f8ddcdc0b1cd7683e2
khangcntt2002 <minhkhang220602@gmail.com>
2024-07-17 20:38:33 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace CShasp_Giaiptbat1WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(97, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 33);
+            label1.TabIndex = 0;
+            label1.Text = "Số A";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(87, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 33);
+            label2.TabIndex = 0;
+            label2.Text = "Số B";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(87, 222);
+            label3.Name = "label3";
+            label3.Size = new Size(110, 33);
+            label3.TabIndex = 0;
+            label3.Text = "Kết Quả";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(122, 352);
+            button1.Name = "button1";
+            button1.Size = new Size(237, 65);
+            button1.TabIndex = 2;
+            button1.Text = "Tính";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(470, 352);
+            button2.Name = "button2";
+            button2.Size = new Size(212, 65);
+            button2.TabIndex = 2;
+            button2.Text = "Thoát";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(377, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(146, 40);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(377, 131);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(146, 40);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(377, 215);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(357, 40);
+            textBox3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(15F, 33F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(774, 881);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Tahoma", 20F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(6, 7, 6, 7);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿namespace CShasp_Giaiptbat1WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Hello, Chào các bạn nha !");
+        Console.WriteLine("ứng dụng giải PT Bật 1");
+
+
+        double a, b;
+        Console.WriteLine("Hãy nhập a : ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Hãy nhập b : ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        string ketqua = GiaiPTBat1(a, b);
+        Console.WriteLine($"ứng dụng giải PT Bật 1 {ketqua} ");
+
+
+    }
+    /// <summary>
+    /// Hàm giải phương trình bật 1
+    /// </summary>
+    /// <param name="a">Đổi số a kiểu double</param>
+    /// <param name="b">Đổi số b kiểu double</param>
+    /// <returns>chuỗi kết quả</returns>
+    private static string GiaiPTBat1(double a, double b)
+    {
+        //
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                return "phương trình có vô số nghiệm";
+
+            }
+            else
+            {
+                return "phương trình vô nghiệm";
+            }
+
+        }
+        else
+        {
+            return "phương trình có 1 nghiệm là: " + (-b / a).ToString();
+        }
+    }
+
+}
+
+
+
